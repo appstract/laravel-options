390d40fc539e2c9c53678c17487cf6f6e8c5d2ec
Mandeep Singh <im@msingh.me>
2017-03-20 01:13:00 +0530

    Update 2017_03_03_100000_create_options_table.php

--- database/migrations/2017_03_03_100000_create_options_table.php
+++ database/migrations/2017_03_03_100000_create_options_table.php
@@ -16,7 +16,7 @@ class CreateOptionsTable extends Migration
         Schema::create('options', function (Blueprint $table) {
             $table->increments('id');
             $table->string('key');
-            $table->string('value');
+            $table->text('value');
         });
     }
 
